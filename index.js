036acdcb919b653a23d9f3135d6413d40860371a
ipanardian <ipan.software@gmail.com>
2016-06-04 17:14:25 +0700

    1.0.6

--- index.js
+++ index.js
@@ -9,77 +9,123 @@
 */
 'use strict'
 
+const VERSION = 'v1.0.6'
+
 if (process.argv.length <= 2) {
-    console.log("Usage: cekresi-cli NOMOR_RESI");
-    process.exit(-1);
+    console.log("Usage: cekresi-cli AWB_NUMBER")
+    process.exit(-1)
 }
 
-var Horseman = require('node-horseman');
-var cheerio = require('cheerio');
-var tabletojson = require('tabletojson');
-var Table = require('cli-table');
-var colors = require('colors');
-var Spinner = require('cli-spinner').Spinner
-var param = process.argv[2];
-var spinner = new Spinner('Checking... %s ');
-spinner.setSpinnerString(7);
-spinner.start();
+var param = process.argv[2]
+if (param == '--version') {
+	console.log(VERSION)
+	process.exit(-1)
+}
+else if (param.length < 10 || !param.match(/^[a-z0-9]+$/i)) {
+	console.log('Invalid AWB Number')
+	process.exit(-1)
+}
 
-var horseman = new Horseman();
+var Horseman = require('node-horseman')
+var cheerio = require('cheerio')
+var tabletojson = require('tabletojson')
+var Table = require('cli-table')
+var colors = require('colors')
+var Spinner = require('cli-spinner').Spinner
+var spinner = new Spinner('Checking... %s ')
+spinner.setSpinnerString(7)
+spinner.start()
+
+var horseman = new Horseman()
 	horseman
 	.userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.3")
 	.on('error', function(message, trace) {
 		console.log(message)
 	})
 	.on('timeout', function(message) {
-		console.log('Timeout')
+		console.log('Timeout\n')
 	})
 	.open('http://cekresi.com/?noresi='+param)
 	.click("#cekresi")
-	.wait(2000)
-	.waitForSelector('#results')
+	.waitFor(function(selector) {
+		return $(selector).is(':visible')
+	}, '#results', true)
 	.evaluate(function() {
-		return document.querySelector('#results').innerHTML;
+		return document.querySelector('#results').innerHTML
 	})
 	.then(function(result) {
-		spinner.stop()
-		var $ = cheerio.load(result)
-		var infoExpedisi = $('h3.top_title').text();
-		
-		if (result != '' && infoExpedisi != '') {
-			var tablesAsJson = tabletojson.convert(result)
-			var infoTable = new Table({head: ['Informasi Pengiriman','','']});
-			var statusTable = new Table({head: ['Tanggal','Lokasi','Keterangan']});
-
-			console.log('\n==================== '+infoExpedisi.red+' =========================\n')
-			for (let tr in tablesAsJson[0]){
-				let row = []
-				for (let td in tablesAsJson[0][tr]) {
-					row.push(tablesAsJson[0][tr][td])
-				}
-				infoTable.push(row)
-			}
-			console.log(infoTable.toString())
-			console.log('\n')
-
-			for (let i = 1; i <= tablesAsJson.length - 1; i++) {
-				for (let tr in tablesAsJson[i]) {
-					let row = []
-					for (let td in tablesAsJson[i][tr]) {
-						row.push(tablesAsJson[i][tr][td])
-					}
-					statusTable.push(row)
-				}
-			}
-			console.log(statusTable.toString())
-		}
-		else {
-			console.log('\nMaaf no resi yang anda masukkan sementara tidak dapat kami proses')
-		}
-		console.log('\nFor details: https://github.com/ipanardian/cekresi-cli')
+		cekresi.load(result).parse()
 	})
 	.finally(function() {
 	    horseman.close()
 	    spinner.stop()
 	    return
-	});
+	})
+
+var cekresi = {
+	result: null,
+	tablesAsJson: null,
+	infoExpedisi: null,
+	el: null,
+	checkResult() {
+		return (this.result != '' && this.infoExpedisi != '' ? true:false)
+	},
+	load(res) {
+		this.parseHTML(res)
+		this.result = res
+		this.infoExpedisi = this.el('h3.top_title').text()
+		return this
+	},
+	parseHTML(html) {
+		this.el = cheerio.load(html)
+	},
+	parseDeliveryInfo() {
+		var deliveryTable = new Table()
+		for (let tr in this.tablesAsJson[0]){
+			let row = []
+			for (let td in this.tablesAsJson[0][tr]) {
+				row.push(this.tablesAsJson[0][tr][td])
+			}
+			deliveryTable.push(row)
+		}
+		print(deliveryTable.toString())
+	},
+	parseActivityInfo() {
+		let header = []
+		let body = []
+		for (let i = 1; i <= this.tablesAsJson.length - 1; i++) {
+			for (let tr in this.tablesAsJson[i]) {
+				let row = []
+				for (let td in this.tablesAsJson[i][tr]) {
+					if (body.length < 1) 
+						header.push(td)
+
+					row.push(this.tablesAsJson[i][tr][td])
+				}
+				body.push(row)
+			}
+		}
+		var activityTable = new Table({head: header})
+		for (let item in body) {
+			activityTable.push(body[item])
+		}
+		print(activityTable.toString())
+	},
+	parse() {
+		if (this.checkResult()) {
+			this.tablesAsJson = tabletojson.convert(this.result)
+			print('\n==================== '+ this.infoExpedisi.yellow +' =========================\n')
+			this.parseDeliveryInfo()
+			print('')
+			this.parseActivityInfo()
+		}
+		else {
+			print('\nMaaf no resi yang anda masukkan sementara tidak dapat kami proses'.yellow)
+		}
+		print('\nFor details: https://github.com/ipanardian/cekresi-cli')
+	}
+}
+
+function print(msg) {
+	console.log(msg)
+}
